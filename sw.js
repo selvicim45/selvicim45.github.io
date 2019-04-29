748a0cb513abc1457fec48df7effe395ba77a9a5
selvicim45 <46985799+selvicim45@users.noreply.github.com>
2019-04-28 21:37:04 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,23 @@
 const staticAssets=[
 './',
-'./index.html'];
+'./index.html'
+'./lightblue.jpg'
+'./lightgold.jpg'];
 
 
-self.addEventListener('install',event => {
-console.log('install');});
+self.addEventListener('install', async event => {
+const cache = await caches.open('pwa-static');
+cache.addAll(staticAssets);
+});
 
-self.addEventListener('fetech',event => {
-console.log('fetch');});
+self.addEventListener('fetch',event => {
+const req = event.request;
+event.respondWith(cacheFirst(req));  
+});
+
+async function cacheFirst(req)
+{
+  const cachedResponse = await cache.match(req);
+  return cachedResponse || fectch(req);
+}
 
