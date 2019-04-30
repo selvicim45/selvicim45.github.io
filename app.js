a8ef8a83cb30db6a86f16e9f920787509e6cd811
selvicim45 <46985799+selvicim45@users.noreply.github.com>
2019-04-29 19:28:12 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 if (navigator.serviceWorker) {
     window.addEventListener('load', () => {
-	   navigator.serviceWorker.register('./service-worker.js')
+	   navigator.serviceWorker.register('sw.js')
 	      .then(registration => console.log('SW registered'))
 		  .catch(err => console.log(`SW not registered - Error: ${err}`))
     })
